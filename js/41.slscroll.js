6e8e6115e1bef326f515a9bb83d01afe9881608e
C. Fahner <christiaanenzo@gmail.com>
2015-12-15 17:05:15 +0100

    add slscroll.js

---
+++ js/41.slscroll.js
@@ -0,0 +1,21 @@
+$(function () {
+
+	var scrollDelay;
+
+	var scrolling = false;
+
+	Object.defineProperty(Slender, "scrolling", {
+		get: function () { return scrolling; }
+	});
+
+	// collects scroll events and fires a single delayed slscroll event
+	$(window).scroll(function () {
+		scrolling = true;
+		clearTimeout(scrollDelay);
+		scrollDelay = setTimeout(function () {
+			scrolling = false;
+			$(".sl-onscroll").trigger("slscroll");
+		}, 250);
+	});
+
+});
